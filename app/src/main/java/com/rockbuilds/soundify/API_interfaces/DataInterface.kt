b7b4d4ca950e0dd70f8eb0617eb7b63fc97ec598
Rakesh <rakeshkulkarni403@gmail.com>
2024-04-15 19:52:52 +0530

    Data class

--- app/src/main/java/com/rockbuilds/soundify/API_interfaces/DataInterface.kt
+++ app/src/main/java/com/rockbuilds/soundify/API_interfaces/DataInterface.kt
@@ -1,4 +1,5 @@
 package com.rockbuilds.soundify.API_interfaces
+import com.rockbuilds.soundify.Data_class.Search.Data
 import com.rockbuilds.soundify.Data_class.album
 import retrofit2.Call
 import retrofit2.http.GET
@@ -11,6 +12,17 @@ interface DataInterface {
    @Headers("X-RapidAPI-Key: 8c0f39b7c8msh5de5f80fe9cfc63p11c578jsn58417fb53ee0","X-RapidAPI-Host: spotify23.p.rapidapi.com")
     @GET("albums")
     fun getAlbumData(@Query("ids") query: String): Call<album>
+
+    @Headers("X-RapidAPI-Key: 8c0f39b7c8msh5de5f80fe9cfc63p11c578jsn58417fb53ee0", "X-RapidAPI-Host: spotify23.p.rapidapi.com")
+    @GET("search/")
+    fun getSearchResults(
+        @Query("q") query: String,
+        @Query("type") type: String = "tracks",
+        @Query("offset") offset: Int = 0,
+        @Query("limit") limit: Int = 8,
+        @Query("numberOfTopResults") numberOfTopResults: Int = 5
+    ): Call<Data>
 }
+
 
 
